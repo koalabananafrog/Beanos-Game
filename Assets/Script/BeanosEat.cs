0a9ad3acd1e9170d5422ef2a02fc04acbd9b8b8f
DESKTOP-48KB3AD\neore <neo.renbris@gmail.com>
2022-10-22 17:53:49 +0200

    spider

--- Assets/Script/BeanosEat.cs
+++ Assets/Script/BeanosEat.cs
@@ -5,9 +5,4 @@ using UnityEngine;
 public class BeanosEat : MonoBehaviour
 {   
     [SerializeField] public Animator AnimatorController;
-   private void OnTriggerEnter(Collision collision){
-    if (collision.gameObject.layer == 11){
-        AnimatorController.SetBool("EatBeanos", true);
-    }
-   }
 }
---
+++ Assets/Script/SpiderEat.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderEat : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other){
+        if (other.gameObject.layer == 11){
+            transform.localScale = new Vector3(10, 10, 10);
+        }
+    }
+}
