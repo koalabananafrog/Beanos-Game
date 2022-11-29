b2e3becfb7e598e827066d6772054f386157f3b3
DESKTOP-48KB3AD\neore <neo.renbris@gmail.com>
2022-11-29 22:45:37 +0100

    no message

--- Assets/Script/PositionFollow1.cs
+++ Assets/Script/PositionFollow1.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class PositionFollow1 : MonoBehaviour
 {
     [SerializeField] private Transform PositionHolder;
+    [SerializeField] private bool freezeAllPosExeptX = false;
+    private Vector3 position;
 
     private void FixedUpdate()
     {
-        transform.position = PositionHolder.transform.position;
+        if(freezeAllPosExeptX == false){
+            transform.position = PositionHolder.transform.position;
+        }
+        else{
+            transform.position = position;
+            position = new Vector3(PositionHolder.transform.position.x, PositionHolder.transform.position.y, PositionHolder.transform.position.z + 5);
+        }
     }
 }
