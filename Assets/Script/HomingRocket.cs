e61946a02f5b4e11de2288ebbd8afbaf4d1c79db
Robert Renbris <robert.renbris@gmail.com>
2023-03-21 13:41:13 +0100

    no message

--- Assets/Script/HomingRocket.cs
+++ Assets/Script/HomingRocket.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+ 
 
 public class HomingRocket : MonoBehaviour
 {
@@ -8,8 +9,9 @@ public class HomingRocket : MonoBehaviour
     public Transform Beanos;
     
     public Transform Course;
-    public float rotationSpeed;
+    public float rotSpeed;
     public float maxAngle;
+    public GameObject BOOM;
     
     
     // Start is called before the first frame update
@@ -26,7 +28,16 @@ public class HomingRocket : MonoBehaviour
     }
 
     private void FixedUpdate(){
+        
         transform.position = Vector3.MoveTowards(transform.position, Course.position, speed * Time.deltaTime);
-        transform.Rotate()
+        
+        Quaternion rotTarget = Quaternion.LookRotation(Beanos.transform.position - transform.position);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotTarget, rotSpeed * Time.deltaTime);
     }
+    private void OnCollisionEnter(){
+        Instantiate(BOOM, transform.position, transform.rotation);
+        Debug.Log("Penis");
+        Destroy(gameObject);
+    }
+
 }
