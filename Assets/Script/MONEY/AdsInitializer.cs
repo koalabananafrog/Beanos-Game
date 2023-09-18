e175cd7c06b3b4711cd1cbfac2b5d9aef0b96ec9
DESKTOP-48KB3AD\neore <neo.renbris@gmail.com>
2023-09-18 20:02:37 +0200

    no message

---
+++ Assets/Script/MONEY/AdsInitializer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] string _androidGameId;
+    [SerializeField] string _iOSGameId;
+    [SerializeField] bool _testMode = true;
+    private string _gameID;
+
+    void Awake(){
+        InitializeAds();
+    }
+    
+}
